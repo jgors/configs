8c2864bab9c3e31f236e56437411e6d236fa281a
jason gors <jason.gors.work@gmail.com>
2013-06-20 15:04:58 -0400

    updated new_repo.sh

--- new_repo.sh
+++ new_repo.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
 
-echo "Installing Vundle in .configs/.vim/bundle/vundle..."
+echo "--> Installing Vundle in .configs/.vim/bundle/vundle..."
 git clone https://github.com/gmarik/vundle.git ~/.configs/.vim/bundle/vundle
 
 cd ~/.configs
 
-echo "Making link for ~/.vimrc file..."
+echo "--> Replacing ~/.vimrc file with link from ~/.configs..."
 rm ~/.vimrc
 ln -s ~/.configs/.vimrc ~/.vimrc
 
-echo "Making link for ~/.vim dir..."
+echo "--> Replacing ~/.vim dir with link from ~/.configs..."
 rm -rf ~/.vim
 ln -s ~/.configs/.vim ~/.vim
 
-echo "Making link for .tmux.conf file..."
+echo "--> Replacing ~/.tmux.conf file  with link from ~/.configs..."
+rm ~/.tmux.conf
 ln -s ~/.configs/.tmux.conf ~/.tmux.conf
 
-echo "Updating all Vundle Bundles..."
+echo "--> Updating all Vundle Bundles..."
 vim +BundleInstall +qall
 
