c544f6336fe72475e3696b38cde7c9fd91461549
jason gors <jason.gors.work@gmail.com>
2013-06-21 12:12:34 -0400

    updated new_repo

--- new_repo.sh
+++ new_repo.sh
@@ -7,15 +7,15 @@ cd ~/.configs
 
 echo "--> Replacing ~/.vimrc file with link from ~/.configs..."
 rm ~/.vimrc
-ln -s ~/.configs/.vimrc ~/.vimrc
+ln -s -v ~/.configs/.vimrc ~/.vimrc
 
 echo "--> Replacing ~/.vim dir with link from ~/.configs..."
 rm -rf ~/.vim
-ln -s ~/.configs/.vim ~/.vim
+ln -s -v ~/.configs/.vim ~/.vim
 
 echo "--> Replacing ~/.tmux.conf file  with link from ~/.configs..."
 rm ~/.tmux.conf
-ln -s ~/.configs/.tmux.conf ~/.tmux.conf
+ln -s -v ~/.configs/.tmux.conf ~/.tmux.conf
 
 echo "--> Updating all Vundle Bundles..."
 vim +BundleInstall +qall
