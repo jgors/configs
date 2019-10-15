312d133a5125b895190695475e9b966e56445df3
jgors <jason.gors.work@gmail.com>
2019-10-15 14:02:24 -0700

    RF: separate out jupyter install/setup into it's own file.

---
+++ jupyter_install.sh
@@ -0,0 +1,15 @@
+echo "\n--> INSTALLING PYTHON PKG jupyter"
+pip3 install jupyter --user --upgrade
+
+echo "\n--> Setting up jupyter to use vim keybindings in notebook"
+echo "--> NOTE:  for this to work, need to make sure this extension is"
+echo "--> installed:  https://github.com/lambdalisue/jupyter-vim-binding"
+jupyter_custom=~/.jupyter/custom
+if [ ! -d $jupyter_custom ]; then
+    echo "\n--> Making" $jupyter_custom
+    mkdir $jupyter_custom
+else
+    echo "--> Updating" $jupyter_custom
+fi
+ln -sfv $configs/.jupyter/custom/custom.js $jupyter_custom
+ln -sfv $configs/.jupyter/custom/custom.css $jupyter_custom
